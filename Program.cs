c4ce70dfa94748b453326d1da50d36aec12b96ad
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-09 10:04:58 +0530

    Add number to Linkedlist

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("\n.....................\n");
+            linkedList.InsertAtParticularPosition(2, 30);
+            linkedList.InsertAtParticularPosition(3, 56);
+            Console.WriteLine("\n.....................\n");
+            linkedList.Display();
+        }
+    }
+}
