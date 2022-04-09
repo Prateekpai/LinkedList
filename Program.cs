51b668305d11054b4a628ab2b5d813336bbf3b51
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-09 10:09:19 +0530

    Delete number in Linkedlist

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            Console.WriteLine("\n.....................................\n");
+            linkedList.Display();
+            Console.WriteLine("\n.....................................\n");
+            linkedList.RemoveFirstNode();
+            Console.WriteLine("\n.....................................\n");
+            linkedList.Display();
+        }
+    }
+}
