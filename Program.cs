c22e6e57cb14542cee0a1a1114007095f015a1f1
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-09 10:14:22 +0530

    Insert Number at Particular Location in Linkedlist

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            Console.WriteLine("\n...........................\n");
+            linkedList.Display();
+            Console.WriteLine("\n...........................\n");
+            linkedList.InsertAtParticularPosition(3, 40);
+            Console.WriteLine("\n...........................\n");
+            linkedList.Display();
+            Console.WriteLine("\n...........................\n");
+        }
+    }
+}
