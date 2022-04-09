efaab2bc17eb129e13c56d50f73395974eb071e7
Prateekpai <prateekvasanthpai@gmail.com>
2022-04-09 10:08:10 +0530

    Inser number in Linkedlist

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LinkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(56);
+            linkedList.Add(70);
+            Console.WriteLine("\n........................\n");
+            linkedList.Display();
+            Console.WriteLine("\n........................\n");
+            linkedList.InsertAtParticularPosition(2, 30);
+            Console.WriteLine("\n........................\n");
+            linkedList.Display();
+            Console.WriteLine("\n........................\n");
+        }
+    }
+}
